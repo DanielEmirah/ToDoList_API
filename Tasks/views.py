a19356db675227f4cae5d86240a28e3472581961
Daniel Emirah <danielemirah@gmail.com>
2026-01-31 20:57:32 +0300

    Création vues taskslist et configuration template de base avec templates pour les listes

--- Tasks/views.py
+++ Tasks/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Task
 
-
+def taskslist(request):
+    tasks = Task.objects.all()
+    return render(request, "Tasks/list.html", {"tasks": tasks})
